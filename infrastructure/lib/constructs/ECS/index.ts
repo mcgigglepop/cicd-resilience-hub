9382fe122fb9fd3e8823b15fc1fae43fa5e9ccbf
cristopher ontiveros <christoph.alano@gmail.com>
2025-01-31 14:53:38 -0600

    removed second target

--- infrastructure/lib/constructs/ECS/index.ts
+++ infrastructure/lib/constructs/ECS/index.ts
@@ -119,10 +119,6 @@ export class ECS extends Construct {
           containerName: `Resilience-Express`,
           containerPort: 80,
         }),
-        this.service.loadBalancerTarget({
-          containerName: `Resilience-Express`,
-          containerPort: 80,
-        }),
       ],
       healthCheck: {
         protocol: Protocol.HTTP,

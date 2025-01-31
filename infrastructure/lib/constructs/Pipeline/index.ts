d964f6b0c5ecc5b511ba2638aec817ee06ff757f
cristopher ontiveros <christoph.alano@gmail.com>
2025-01-31 11:47:08 -0600

    added value

--- infrastructure/lib/constructs/Pipeline/index.ts
+++ infrastructure/lib/constructs/Pipeline/index.ts
@@ -168,8 +168,8 @@ export class PipelineStack extends Construct {
           executionNamePrefix: 'codepipeline',
           stateMachineInput: {
             input: TaskInput.fromObject({
-              StackArn: "arn:aws:cloudformation:us-east-1:412791426734:stack/ResilienceInfrastructureStack/e7dd7160-dfec-11ef-bd71-1274fecdafe9",
-              AppArn: "arn:aws:resiliencehub:us-east-1:412791426734:app/e0297334-f5c0-474b-a227-89f8f78230a0"
+              "StackArn": "arn:aws:cloudformation:us-east-1:412791426734:stack/ResilienceInfrastructureStack/e7dd7160-dfec-11ef-bd71-1274fecdafe9",
+              "AppArn": "arn:aws:resiliencehub:us-east-1:412791426734:app/e0297334-f5c0-474b-a227-89f8f78230a0"
             }).value,
           },
         }),
